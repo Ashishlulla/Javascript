a1a79b12824bb2ca001748153e18487feb91ff19
Ashish Rajesh Lulla <lullaashish2807@gmail.com>
2022-02-19 00:40:45 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,63 @@
+// type of opeator
+
+// --- data types ---
+// String = "ashish"
+// number = 1,2,3,4,5
+// booleans
+
+
+// undefined (const cannot be undefined)
+// let firstName;
+// console.log(typeof firstName)
+// firstName = "Ashish"
+// console.log(typeof firstName, firstName)
+let num1 = "7";
+let num2 = 7;
+// console.log(num1 <= num2)
+
+// == vs === or != or !==
+// console.log(num1 == num2)
+// console.log(num1 === num2)
+// console.log(num1 != num2)
+// console.log(num1 !== num2)
+// symbol
+
+// Bigint()
+// let myNum = BigInt(12303434223232332332332323232323232);
+// console.log(myNum)
+// console.log(Number.MAX_SAFE_INTEGER)
+
+// null
+// let myVariable = null
+// console.log(typeof myVariable)
+// console.log(typeof null)  // bug, error
+
+// let age = 22
+// let name = "Ashish"
+// console.log(typeof age)
+// console.log(typeof name)
+
+
+// convert number to string
+
+// age = age + "";
+// console.log(typeof age)
+// convert string to number
+
+// let myStr = +"22";
+// console.log(typeof myStr)
+
+// string concatenation 
+
+// let string1 = "Ashish"
+// let string2 = "Lulla"
+
+// let full_name = string1 + " " + string2
+// console.log(full_name)
+
+// let aboutme ="my name is "+ string1
+
+// template string
+// let aboutme =`my first name is ${string1} and my lastname is ${string2}`
+// console.log(aboutme) 
+
