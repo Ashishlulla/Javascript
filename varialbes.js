dbe577b80e4b3b6879e6f59221c91e2d53790256
Ashish Rajesh Lulla <lullaashish2807@gmail.com>
2022-02-19 00:40:21 +0530

    Add files via upload

---
+++ varialbes.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// This is a hello world program
+console.log("Hello World");
+
+// console.log can print something on console
+
+// Decalare Variables
+
+var firstname = "Ashish"
+
+// Use a Variables
+console.log(firstname)
+
+// change value
+
+firstname = "Lulla"
+console.log(firstname)
+
+// rules for naming a variable
+
+// you cannot start with Number
+// example:
+// 1value :- "Invalid"
+
+var value1 = 10
+console.log(value1**2)
+// value1 := "valid"
+
+// you can use a underscore a dollar Symbol
+
+// _firstname: "valid"
+// first_name : "valid"
+
+// $firstname: "valid"
+// first$name : "valid"
+
+// cannot use a space 
+
+// convention is start with smallercase and use camelcase
+
