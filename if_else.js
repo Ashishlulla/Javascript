099b0b0b08d9c26248d88121236f7f1d65a70577
Ashish Rajesh Lulla <lullaashish2807@gmail.com>
2022-02-24 11:52:16 +0530

    Add files via upload

---
+++ if_else.js
@@ -0,0 +1,155 @@
+// // if else condition
+
+// let age = 17
+
+// if (age >= 18){
+//     console.log("you can play this game")
+// }else{
+//     console.log("you can't play this game")
+// }
+
+// let num = 14
+// if (num % 2 == 0){
+//     console.log("Even")
+// }else{
+//     consolelog("Odd")
+// }
+
+//  truthy and falsy values
+
+// falsy values
+
+// false
+// ""
+// null
+// undefined
+// 0
+
+// let firstName = null;
+// if(firstName){
+//     console.log(firstName)
+// }else{
+//     console.log("firstName is Kinda empty")
+// }
+
+// Ternary Operator
+
+// let age = 15;
+// let drink = age >=5 ? "Coffee":"milk"
+
+// if (age >= 5){
+//     drink = "coffee"
+// }else{
+//     drink = "milk"
+// }
+// console.log(drink)
+
+// and or operator
+// and (&&)
+// let firstName = "ashish"
+// let age = 19
+// if (firstName[0] === "a" && age > 18){
+//     console.log("your Name starts With A and your Above 18")
+// }
+
+// let firstName = "Ashish"
+// let age = 19
+// if (firstName[0] === "a" || age > 18){
+//     console.log("your Name starts With A and your Above 18")
+// }
+
+// Nested  If-else :
+
+// let win = 19
+// let userGuess = +prompt("Gues a number");
+// if (userGuess == win){
+//     console.log("Usrr Guess is right")
+// }else{
+//     if (userGuess < 19){
+//         console.log("Too low")
+//     }else{
+//         console.log("Too High")
+//     }
+// }
+
+// if elseif else
+
+// let temp = 40;
+
+// if (temp < 0){
+//     console.log("too cold");
+// }else if (temp < 20){
+//     console.log("Cold");
+// }else if (temp < 30){
+//     console.log("Normal");
+// }else{
+//     console.log("Turn on AC");
+// }
+
+
+// Switch Statement
+
+// let day = -1;
+// switch(day){
+//     case 0:
+//         console.log("Sunday");
+//         break;
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     default:
+//         console.log("Invalid Input")
+// }
+
+// While Loop
+
+// let i = 0;
+
+// while (i < 11){
+//     console.log(i)
+//     i++;
+// }
+
+
+// //  Example:
+
+// let num = 10;
+// let sum = 0;
+// let i = 1;
+
+// while (i <= num){
+//     sum = sum + i;
+//     i++;
+// }
+// console.log(sum);
+
+// For Loop 
+// let sum = 0;
+// for(let i = 0; i <= 10; i++){
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+// Do while Loop
+
+// let i = 0;
+
+// do{
+//     console.log(i)
+//     i++;
+// }while (i < 11)
